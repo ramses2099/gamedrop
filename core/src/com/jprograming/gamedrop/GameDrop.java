7a34b9fbb5e028760cf456748c17da1ba435ee3f
ramses2099 <ramses2099@gmail.com>
2024-01-19 18:37:33 -0400

    final exercise

--- core/src/com/jprograming/gamedrop/GameDrop.java
+++ core/src/com/jprograming/gamedrop/GameDrop.java
@@ -8,8 +8,13 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class GameDrop extends ApplicationAdapter {
 
@@ -17,14 +22,17 @@ public class GameDrop extends ApplicationAdapter {
     final float HEIGHT = 480;
     final float BUCKET_WIDTH = 64;
     final float BUCKET_HEIGHT = 64;
+    final float RAINDROP_WIDTH = 64;
+    final float RAINDROP_HEIGHT = 64;
     SpriteBatch batch;
     Texture dropImage;
     Texture bucketImage;
     Sound dropSound;
     Music rainMusic;
     OrthographicCamera camera;
-
     Rectangle bucket;
+    Array<Rectangle> raindrops;
+    long lastDropTime;
 
     @Override
     public void create() {
@@ -49,6 +57,9 @@ public class GameDrop extends ApplicationAdapter {
         bucket.y = 20;
         bucket.width = BUCKET_WIDTH;
         bucket.height = BUCKET_HEIGHT;
+
+        raindrops = new Array<Rectangle>();
+        spawnRaindrop();
     }
 
     @Override
@@ -56,14 +67,36 @@ public class GameDrop extends ApplicationAdapter {
         ScreenUtils.clear(0, 0, 0.2f, 1);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
+        // DRAW
         batch.begin();
         batch.draw(bucketImage, bucket.x, bucket.y);
+        for (Rectangle rainDrop : raindrops) {
+            batch.draw(dropImage, rainDrop.x, rainDrop.y);
+        }
         batch.end();
+        // UPDATE
+        inputBucket();
+        //
+        if (TimeUtils.nanoTime() - lastDropTime > 100000000) spawnRaindrop();
+        //
+        for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext();) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if(raindrop.y +  RAINDROP_HEIGHT < 0)iter.remove();
+            if(raindrop.overlaps(bucket)){
+                dropSound.play();
+                iter.remove();
+            }
+        }
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
 
     }
 
@@ -73,6 +106,17 @@ public class GameDrop extends ApplicationAdapter {
         //
         if (bucket.x < 0) bucket.x = 0;
         if (bucket.x > WIDTH - BUCKET_WIDTH) bucket.x = WIDTH - BUCKET_WIDTH;
+    }
+
+    //
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, WIDTH - BUCKET_WIDTH);
+        raindrop.y = 480;
+        raindrop.width = RAINDROP_WIDTH;
+        raindrop.height = RAINDROP_HEIGHT;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
     }
 
 }
